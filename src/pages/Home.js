ebf08597ea66202e73bf2958a1db6732585e838c
Jarrett Cee <jarrettcura@gmail.com>
2024-06-10 21:21:43 -0500

    Completed Week 1 Project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import AddTodo from "../component/AddTodo.js";
+import Todos from "../component/todos.js"; 
+import "./Home.css";
 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
@@ -6,6 +9,7 @@ class Home extends Component {
     super();
     this.state = {
       // create your empty list here call it todos.
+      todos: []
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -27,8 +31,10 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
-        <h1>Todo's </h1>
+        <br></br>
+        <h1>Todo List</h1>
+        <AddTodo addTodo={this.addTodo} />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
